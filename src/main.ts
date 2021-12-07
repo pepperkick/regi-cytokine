5b0f7eb429c9222c1f0c3ee8585420cc35534952
punteroo <mazalucas@hotmail.com>
2021-12-07 18:18:31 -0300

    structure and microservice initialization

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,19 @@
 import { NestFactory } from '@nestjs/core';
+import { Transport } from '@nestjs/microservices';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+
+  app.connectMicroservice({
+    transport: Transport.TCP,
+    options: {
+      host: '0.0.0.0',
+      port: 3110,
+    },
+  });
+
+  await app.startAllMicroservices();
+  await app.listen(3100);
 }
 bootstrap();
