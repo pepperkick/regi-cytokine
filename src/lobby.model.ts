f59df250364d175d68f46c9ca4e77f583b9d47e4
punteroo <mazalucas@hotmail.com>
2021-12-08 18:11:30 -0300

    Changes - Fix eslint not working by downgrading - Run eslint fix across files - Remove hardcoded token

--- src/lobby.model.ts
+++ src/lobby.model.ts
@@ -1,30 +1,29 @@
-/* eslint-disable prettier/prettier */
-/*import { Prop, Schema, SchemaFactory } from "@nestjs/mongoose";
-import { Player } from "../matches/match-player.interfaace";
-import { RoleRequirement } from "../../objects/role.interface";
-import { Document } from "mongoose";
-import { LobbyType } from "../../objects/lobby-type.enum";
-import { LobbyStatus } from "./lobby-status.enum";
-
-@Schema()
-export class Lobby extends Document {
-	@Prop({ type: Date })
-	createdAt: Date
-
-	@Prop()
-	match: string
-
-	@Prop({ type: String })
-	status: LobbyStatus
-
-	@Prop({ type: String })
-	type: LobbyType
-
-	@Prop({ type: Object })
-	queuedPlayers: Player[]
-
-	@Prop({ type: Object })
-	requirements: RoleRequirement[]
-}
-
-export const LobbySchema = SchemaFactory.createForClass(Lobby);*/
+// import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
+// import { Player } from '../matches/match-player.interfaace';
+// import { RoleRequirement } from '../../objects/role.interface';
+// import { Document } from 'mongoose';
+// import { LobbyType } from '../../objects/lobby-type.enum';
+// import { LobbyStatus } from './lobby-status.enum';
+//
+// @Schema()
+// export class Lobby extends Document {
+//   @Prop({ type: Date })
+//   createdAt: Date;
+//
+//   @Prop()
+//   match: string;
+//
+//   @Prop({ type: String })
+//   status: LobbyStatus;
+//
+//   @Prop({ type: String })
+//   type: LobbyType;
+//
+//   @Prop({ type: Object })
+//   queuedPlayers: Player[];
+//
+//   @Prop({ type: Object })
+//   requirements: RoleRequirement[];
+// }
+//
+// export const LobbySchema = SchemaFactory.createForClass(Lobby);
--- src/lobby.service.ts
+++ src/lobby.service.ts
@@ -1,8 +1,7 @@
-/* eslint-disable prettier/prettier */
-import { Logger } from "@nestjs/common";
+import { Logger } from '@nestjs/common';
 
 export class LobbyService {
-    private readonly logger = new Logger(LobbyService.name);
+  private readonly logger = new Logger(LobbyService.name);
 
-    //constructor ()
+  //constructor ()
 }
