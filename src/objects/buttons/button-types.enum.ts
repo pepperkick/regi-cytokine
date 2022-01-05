03990f5896ac56bf7d6744119a7107c330e87e03
punteroo <mazalucas@hotmail.com>
2022-01-05 17:35:41 -0300

    added button types
    - for random and captain based distribution lobbies, there will be only 2 buttons: Queue and Unqueue.
    - captains will be able to pick players later on (this is TODO, as for now we'll implement a random distribution logic before moving on to the other ones)

---
+++ src/objects/buttons/button-types.enum.ts
@@ -0,0 +1,7 @@
+export enum ButtonType {
+  // Queue is only present on a RANDOM distribution lobby.
+  QUEUE = 'queue',
+  // Present in all types of lobbies
+  UNQUEUE = 'unqueue',
+  // TODO: Add support for other Lobby Distribution Methods
+}
