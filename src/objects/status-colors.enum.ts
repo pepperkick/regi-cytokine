2f7f633da4bef07331f7bf295b333d307b19ab1e
Lucas <mazalucas@hotmail.com>
2022-01-15 17:26:19 -0300

    New Embed Status colors
    - Whenever a status change occurs, the Message's Embed is updated with a different color for each status.

---
+++ src/objects/status-colors.enum.ts
@@ -0,0 +1,8 @@
+// These colors are used for the Embeds in the Discord messages.
+
+export enum StatusColors {
+  'WAITING_FOR_REQUIRED_PLAYERS' = 0x787878,
+  'DISTRIBUTING' = 0xe6800b,
+  'DISTRIBUTED' = 0x0b79e6,
+  'LOBBY_READY' = 0x0ee31f,
+}
