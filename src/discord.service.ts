1f450bfe69aaf11b1f94b268378262b5c2319ca3
Lucas <mazalucas@hotmail.com>
2022-01-15 17:25:27 -0300

    Change Client scope on DiscordService
    - Changed the Client scope on DiscordService from run() to private.
    - Added getter for the Client
    - Added getMessage
       - Snatches a Message object being passed its ID.

--- src/discord.service.ts
+++ src/discord.service.ts
@@ -1,23 +1,46 @@
 import { Logger } from '@nestjs/common';
-import { ButtonInteraction, Intents, Interaction } from 'discord.js';
+import { Intents, Interaction, Message, TextChannel } from 'discord.js';
 import { Client } from 'discordx';
 
 import * as config from '../config.json';
-import { ButtonType } from './objects/buttons/button-types.enum';
-import { Player } from './objects/match-player.interface';
 
 export class DiscordService {
   private readonly logger = new Logger(DiscordService.name);
+  private bot: Client;
 
   // On instantiation, execute logic to run the bot.
   constructor() {
     this.run();
   }
 
+  /**
+   * Get the Bot Client
+   */
+  public getClient(): Client {
+    return this.bot;
+  }
+
+  /**
+   * Gets a Discord Message object from the message ID.
+   */
+  async getMessage(
+    messageId: string,
+  ): Promise<Message<true> | Message<boolean>> {
+    // Get the Guild first
+    const guild = await this.bot.guilds.fetch(config.client.guild);
+
+    // Search for the channel inside the guild
+    const channel = await guild.channels.fetch(config.client.channel);
+
+    // Get the message from the channel
+    if (channel instanceof TextChannel)
+      return await channel.messages.fetch(messageId);
+  }
+
   // Run the bot
   private async run() {
     // Create a new Discord client
-    const bot = new Client({
+    this.bot = new Client({
       intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES],
       botGuilds: [config.client.guild],
       silent: true,
@@ -27,28 +50,28 @@ export class DiscordService {
     //////////////////
     //
     // ready
-    bot.once('ready', async () => {
+    this.bot.once('ready', async () => {
       // Load all commands available
-      await bot.initApplicationCommands({
+      await this.bot.initApplicationCommands({
         guild: { log: true, disable: { delete: true } },
         global: { log: true, disable: { delete: true } },
       });
 
       // Load permissions
-      await bot.initApplicationPermissions(true);
+      await this.bot.initApplicationPermissions(true);
 
       // Log the bot is ready
       console.log('The Discord bot is ready!');
     });
 
     // interactionCreate
-    bot.on('interactionCreate', (interaction: Interaction) => {
+    this.bot.on('interactionCreate', (interaction: Interaction) => {
       if (interaction.guild.id !== config.client.guild) return;
 
-      bot.executeInteraction(interaction);
+      this.bot.executeInteraction(interaction);
     });
 
     // Login to the bot user with the token provided.
-    bot.login(config.client.token);
+    this.bot.login(config.client.token);
   }
 }
