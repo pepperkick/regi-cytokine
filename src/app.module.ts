6bf9da54dea8c49185a4264e9a2be1e0eb21c132
Lucas <mazalucas@hotmail.com>
2022-01-15 22:15:33 -0300

    Modules Modifications
    - For LobbyService to use DiscordService

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,18 +4,13 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { LobbyModule } from './modules/lobby/lobby.module';
 import { LobbyCommand } from './commands/lobby.command';
-import { DiscordService } from './discord.service';
 
 import * as config from '../config.json';
-import {
-  DiscordInfo,
-  DiscordInfoSchema,
-} from './objects/discord-info/discord-info.model';
 
 @Module({
   imports: [LobbyModule, MongooseModule.forRoot(config.mongodbUri)],
   controllers: [AppController],
-  providers: [AppService, DiscordService, LobbyCommand],
-  exports: [DiscordService],
+  providers: [AppService, LobbyCommand],
+  exports: [],
 })
 export class AppModule {}
